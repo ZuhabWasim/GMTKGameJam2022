f4db655ad3185ad1e2d930a97f66063b80fa630d
thehg0902 <thehg0902@gmail.com>
2022-07-15 22:35:19 -0400

    P1 Loadout Pannel
    Player 1 loadout Pannel Open / Retract

---
+++ Assets/Scripts/UI/LerpMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class LerpMovement : MonoBehaviour
+{
+    public GameObject TargetPos;
+    public GameObject InitPos;
+    
+    public GameObject LoadOutPos;
+
+    public float speed;
+    private float elapsedTime;
+
+    
+
+    private bool Toggle = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        elapsedTime += Time.deltaTime;
+        float percentageComplete = elapsedTime / speed;
+
+        if (Toggle)
+        {
+            LoadOutPos.transform.position = Vector3.Lerp(InitPos.transform.position, TargetPos.transform.position, percentageComplete);
+        }else if (!Toggle)
+        {
+            LoadOutPos.transform.position = Vector3.Lerp(LoadOutPos.transform.position, InitPos.transform.position, percentageComplete);
+        }
+        //Debug.Log(Toggle);
+
+
+    }
+    
+    public void LoadOutSliding()
+    {
+        if (Toggle)
+        {
+            Toggle = false;
+        }else if (!Toggle)
+        {
+            Toggle = true;
+        }
+    }
+
+
+
+
+}
