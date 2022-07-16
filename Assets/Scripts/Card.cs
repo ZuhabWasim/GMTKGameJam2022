79e532dcc451e149154a69ead067f5b66e7beaab
Valerie Roussel <59619956+ValerieRoussel@users.noreply.github.com>
2022-07-16 18:34:51 -0400

    Started to make unique cards

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,6 +1,9 @@
 using System;
 using UnityEngine;
 
+using UnityEngine.UI;
+using TMPro;
+
 public struct Tier
 {
     public int start;
@@ -23,11 +26,34 @@ public struct Tier
 [Serializable]
 public class Card : MonoBehaviour
 {
+    public Image cardColor;
+    public Image img;
+    public TMP_Text txtTitle;
+    public TMP_Text txtDescription;
+    
+    
     public int id;
     public int tier;
 
     public string name;
     public string description;
+
+
+    public int physDamage=0;
+    public int fireDamage=0;
+    public int enerDamage=0;
+
+    public int physShield=0;
+    public int iceShield=0;
+    public int enerShield=0;
+
+    public int poison=0;
+    public int sleepy=0;
+    public int fragile=0;
+    public int speed=0;
+    public int strength=0;
+    public int focus=0;
+
     
     public Card(int id = 0, int tier = 0, string name = "", string description = "")
     {
@@ -35,6 +61,14 @@ public class Card : MonoBehaviour
         this.id = id;
         this.name = name;
         this.description = description;
+    }
+
+    void Start() {
+        //Assign color based on tier
+        cardColor.color = new Color32(100, 100, 100, 255);
+
+        txtTitle.text = name;
+        txtDescription.text = description;
     }
     
     protected virtual void CardEffects()
